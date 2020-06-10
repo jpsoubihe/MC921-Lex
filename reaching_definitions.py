0d3fcc8f7eabf58c24f6d033590b6c7e7e126eba
Joao Paulo Soubihe <j151106@students.ic.unicamp.br>
2020-06-09 22:15:49 -0300

    reaching definitions done - needing some tests

--- reaching_definitions.py
+++ reaching_definitions.py
@@ -11,47 +11,61 @@ class Analyzer():
         self.out_set = {}
         self.in_set = {}
         self.kill_set = {}
+        self.scope = ''
 
-    def gen(self, instruction):
+    def gen(self, index, instruction):
         '''
         Given an instruction it returns the respective gen[n] set.
+        Maybe needs some refinement. (return and parameter)
         '''
         if instruction[0].startswith('store'):
-            return instruction[2]
+            if instruction[1].startswith('%' + self.scope + '_'):
+                pass
+            elif instruction[2].endswith('return'):
+                pass
+            else:
+                return index, instruction[2][1:]
 
+    def store_kill(self, index, definition):
+        attribute = definition[len(definition) - 1]
+        return index, attribute[1:]
 
-    def kill(self, sentence, kill_set, block):
+    def kill(self, sentence, block):
         '''
         Given a sentence and the block being analyzed, it returns the respective kill[n] set.
         '''
         kills = []
         if sentence[0].startswith('store'):
-            for definition in block:
+            for index, definition in enumerate(block):
                 if definition[0].startswith('store') and definition != sentence:
                     if definition[2] == sentence[2]:
-                        kills.append(definition)
+                        kills.append(self.store_kill(index, definition))
         if len(kills) > 0:
             return kills
 
-    def generate_in(self, in_set, out_set, index):
-        set = []
+    def generate_in(self,  out_set, index):
+        in_set = set()
         for predecessor in range(index):
-            if out_set.__contains__(predecessor):
-                set.append(out_set[predecessor])
-        in_set[index] = set
+            if out_set.__contains__(predecessor) and out_set[predecessor] is not None:
+                out_set_set = set(out_set[predecessor])
+                in_set = in_set | out_set_set
         return in_set
 
-    def generate_out(self, in_set, out_set, index):
+    def generate_out(self, in_set, index):
+        in_set_set = set(in_set[index])
         if self.gen_set[index] is not None:
+            gen = set([self.gen_set[index][0]])
             if self.kill_set[index] is not None:
-                return self.gen_set[index] + (in_set[index] - self.kill_set[index])
+                kill = set([self.kill_set[index][0][0]])
+                return list(gen | (in_set_set - kill))
             else:
-                return self.gen_set[index] + in_set[index]
+                return list(gen | in_set_set)
         else:
             if self.kill_set[index] is not None:
-                return in_set[index] - self.kill_set[index]
+                kill = set([self.kill_set[index][0][0]])
+                return list(in_set_set - kill)
             else:
-                return in_set[index]
+                return list(in_set_set)
 
     def generate_in_out(self):
         '''
@@ -63,15 +77,15 @@ class Analyzer():
         in_set = self.in_set
         out_set = self.out_set
         while index < len(self.kill_set):
-            in_set[index] = self.generate_in(in_set, out_set, index)
-            out_set[index] = self.generate_out(in_set, out_set, index)
+            in_set[index] = self.generate_in(out_set, index)
+            out_set[index] = self.generate_out(in_set, index)
             index += 1
         '''
                 Applies the values of in and out sets of each sentence of the block - single iteration. Returns a boolean indicating if the sets changed or not.
                 in[n] = U out[p] (p -> predecessors)
                 out[n] = gen[n] U (in[n] - kill[n])
         '''
-        if in_set == self.in_set and out_set == self.out_set:
+        if in_set != self.in_set and out_set != self.out_set:
             return True
         self.in_set = in_set
         self.out_set = out_set
@@ -82,10 +96,6 @@ class Analyzer():
         Parses the vector of instructions of a block into a tuple. ToDo: Check if structure of the sets for analysis shoould really be made here.
         '''
         v = []
-        in_set = {}
-        out_set = {}
-        gen_set = {}
-        kill_set = {}
 
         # 1st iteration - setting gens and kills
         for instruction in block:
@@ -97,8 +107,8 @@ class Analyzer():
 
         index = 0
         for instruction in v:
-            self.gen_set[index] = self.gen(instruction)
-            self.kill_set[index] = self.kill(instruction, kill_set, v)
+            self.gen_set[index] = self.gen(index, instruction)
+            self.kill_set[index] = self.kill(instruction, v)
             index += 1
 
         modified = True
@@ -132,6 +142,7 @@ class Analyzer():
         Executes the reaching definitions analysis. ToDo: Maybe it will be refactored (see Parse function for more infos)
         '''
         for block_list in self.blocks:
+            self.scope = block_list[0].label
             for block in block_list:
                 block_set = self.parse(block)
                 # gen(block_set)
