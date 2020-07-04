794acd3ebc5576efc2fbea31a34b9d52442bad1d
Joao Paulo Soubihe <j151106@students.ic.unicamp.br>
2020-07-04 19:37:16 -0300

    newer version of llvm code generation

--- llvm_code.py
+++ llvm_code.py
@@ -1,5 +1,7 @@
 from llvmlite import ir
 
+import uc_block
+
 int_type = ir.IntType(32)
 float_type = ir.FloatType()
 void_type = ir.VoidType
@@ -16,6 +18,7 @@ class LLVM_builder():
 
     def __init__(self, module):
         self.module = module
+        self.builder = None
         self.functions = []
 
     def preparation(self, function_blocks):
@@ -32,13 +35,31 @@ class LLVM_builder():
             for arg_type in range(0, len(func_args) - 1, 2):
                 args.append(to_type(func_args[arg_type]))
         fnty = ir.FunctionType(to_type(func_type), args)
-        ir.Function(self.module, fnty, function)
+        fn = ir.Function(self.module, fnty, function)
         self.functions.append(function_blocks[0].instructions[0].split(' ')[1])
+        return fn
+        # self.builder = ir.IRBuilder(fn.append_basic_block(function))
+
+    def build_alloc(self, instruction):
+        self.builder.alloca(ir.IntType(32), name=instruction.split(' ')[3])
+
+    def append_instructions(self, instructions):
+        for inst in instructions:
+            if inst[2:].startswith('alloc_'):
+                self.build_alloc(inst)
+
+    def append_block(self, function, block):
+        if isinstance(block, uc_block.BasicBlock):
+            self.builder = ir.IRBuilder(function.append_basic_block(block.label))
 
 
     def generate_code(self, blocks):
         '''
         Receive all blocks on a list of lists and convert the respective instructions to LLVM IR
         '''
+
         for function in blocks:
-            self.preparation(function)
+            func = self.preparation(function)
+            for block in function:
+                self.append_block(func, block)
+                self.append_instructions(block.instructions)
